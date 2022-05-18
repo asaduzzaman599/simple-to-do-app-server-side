6bea9b1cbe1c97d49bd52a868b33a2e26103045a
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-05-19 02:37:34 +0600

    express.json bug fixed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000
 
 //middleware
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tpwhz.mongodb.net/?retryWrites=true&w=majority`;
@@ -26,7 +26,8 @@ async function run() {
         // create a document to insert
         app.post('/task', async (req, res) => {
             const task = req.body
-            const result = await collectionTask.insertOne(doc);
+            console.log(task)
+            const result = await collectionTask.insertOne(task);
             res.send(result)
         })
 
@@ -35,7 +36,6 @@ async function run() {
             const result = await collectionTask.find({}).toArray();
             res.send(result)
         })
-
         // update a task  
         app.put('/task/:taskId', async (req, res) => {
             const { taskId } = req.params
@@ -63,7 +63,6 @@ async function run() {
             }
             res.send(result)
         })
-
 
 
 
